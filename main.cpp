96cd5835d4e35149dba4e2d32af0b1304351dd6e
Angrybeef1 <136150862+Angrybeef1@users.noreply.github.com>
2024-09-29 20:18:45 -0700

    First Commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+//comsc 210 | lab 14 | Robert Stonemetz
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class Color {
+private:
+    int red;
+    int green;
+    int blue;
+
+public:
+    // Constructors
+    Color() : red(0), green(0), blue(0) {}
+    Color(int r, int g, int b) : red(r), green(g), blue(b) {}
+
+    // Setters
+    void setRed(int r) { red = r; }
+    void setGreen(int g) { green = g; }
+    void setBlue(int b) { blue = b; }
+
+    
+    
+};
+
+int main() {
+   
+    return 0;
+}
